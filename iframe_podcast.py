fdb0b1efbeea4f29e7f5ec34cabdaf3d4347eef2
arang129 <48246455+arang129@users.noreply.github.com>
2026-02-13 21:37:42 +0800

    Fix iframe source URL for podcast service

--- iframe_podcast.py
+++ iframe_podcast.py
@@ -80,7 +80,7 @@ TEMPLATE = """\
     </style>
 </head>
 <body>
-    <iframe src="https://jupyter2.yunlab.app/services/podcasts-learning/" 
+    <iframe src="https://jupyter.yunlab.app/services/podcasts-learning/" 
             style="width: 100vw; height: 100vh; border: none;">
     </iframe>
 </body>
